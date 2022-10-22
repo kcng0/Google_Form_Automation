b452ac6694f9394ee57411a430fe6fbfb4565501
kcng0 <79506569+kcng0@users.noreply.github.com>
2022-10-22 19:26:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ auto_submit = automate_function.FormAutomate(DRIVER_PATH)
 
 
 for i in range(10):
-    auto_submit.open_browser("https://forms.gle/5z1NtErAioMQZDhd6")
+    auto_submit.open_browser("Your Google Form Link Here")
     yes_button = auto_submit.get_button('//*[@id="i5"]/div[3]/div')
     no_button = auto_submit.get_button('//*[@id="i8"]/div[3]/div')
     submit_button = auto_submit.get_button('//*[@id="mG61Hd"]/div[2]/div/div[3]/div/div[1]/div/span/span')
